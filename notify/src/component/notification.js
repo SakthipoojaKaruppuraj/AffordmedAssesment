5e3267478f00cc55408632f1bbce7e3fa2792a4d
“SakthipoojaKarupuuraj” <sakthipoojakaruppuraj@gmail.com>
2026-06-30 12:10:42 +0530

    Notification updated

--- notify/src/component/notification.js
+++ notify/src/component/notification.js
@@ -1,24 +1,22 @@
-const notification [
+const notifications = [
   {
-    id : '1',
-    type : 'result' ,
-    message : 'mid-sem' ,
-    timestamp : '2026-04-2 17:51:30'
-  }
-
+    id: "1",
+    type: "Result",
+    message: "mid-sem",
+    timestamp: "2026-04-02 17:51:30",
+  },
   {
-    id :'2',
-    type :'placements',
-    message : 'CSX corporate hiring'
-    timestamp : '2026-04-02 17:51:18'
-  }
-
+    id: "2",
+    type: "Placement",
+    message: "CSX Corporate Hiring",
+    timestamp: "2026-04-02 17:51:18",
+  },
   {
-    id : '3' ,
-    type :"event" ,
-    message : 'farewell' ,
-
-  }
-
+    id: "3",
+    type: "Event",
+    message: "Farewell",
+    timestamp: "2026-04-01 10:30:00",
+  },
 ];
-export default notification;
+
+export default notifications;
