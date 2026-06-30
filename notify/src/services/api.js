e568df6179cb7d10e7dcdb0e07e284e4874502cb
“SakthipoojaKarupuuraj” <sakthipoojakaruppuraj@gmail.com>
2026-06-30 12:42:53 +0530

    error fixed

--- notify/src/services/api.js
+++ notify/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseUrl = "http://4.224.186.213/evaluation-sevice/"
+    baseURL = "http://4.224.186.213/evaluation-service/"
 });
 
 export const getnotifications = async(token) => {
     const response = await API.get("/notifications",{
         headers:{
-            Authorisation : 'Bearer ${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzYWt0aGlwb29qYTQwNDdAZ21haWwuY29tIiwiZXhwIjoxNzgyNzk4NzA5LCJpYXQiOjE3ODI3OTc4MDksImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiJmNzg2MTVkYi1jNDkwLTRjMGYtODI2ZS1hM2EzNmYwYWFkOWYiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJzYWt0aGkgcG9vamEgayIsInN1YiI6IjJhNmRjOWYyLTBlZTQtNDI5Zi04OTRlLTU2ZjM4ZTI2MzdmZiJ9LCJlbWFpbCI6InNha3RoaXBvb2phNDA0N0BnbWFpbC5jb20iLCJuYW1lIjoic2FrdGhpIHBvb2phIGsiLCJyb2xsTm8iOiIyM2FpYjE4IiwiYWNjZXNzQ29kZSI6IldqTnlDVCIsImNsaWVudElEIjoiMmE2ZGM5ZjItMGVlNC00MjlmLTg5NGUtNTZmMzhlMjYzN2ZmIiwiY2xpZW50U2VjcmV0IjoiS01UYUV2eWd1Q2ZqWHJBayJ9.jY4yqnp5XSYn4LeToko1V6ERtCEYT0uKQLhh0Hf3fbA}',
+            Authorization : 'Bearer ${token}',
         }
     });
     return response.data.notification;
